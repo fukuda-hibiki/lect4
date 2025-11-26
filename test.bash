5daa86f3f1b7e35b400d38c49f217ef6868b15ba
fukuda-hibiki <j3358dlo@sit.ac.jp>
2025-11-26 14:35:27 +0900

    test.bashにtestを追加

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SPDX-FileCopyright: 2025 Hibiki Fukuda
+#SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "${res}" = 0 ] && echo OK
+exit $res
+
